azat-archive/avro

commit e8c61ba4f0051a76d4bf2e2d4391ec92a6e90f4f

AVRO-3162: Use Argparse to Manage Arguments (#1270)
Improve the python cli, consisting mainly of scripts/avro and avro.tool, by implementing argparse, and a `__main__.py` module.

- argparse handles arity and type exceptions more fluently at invocation time than can easily be done with sys.argv.
- argparse handles sub-commands natively, especially in python 3.7 and up.
- `__main__.py` dispatch (along with setuptools `entry_points.console_scripts` at install time) allows the cli to be invoked with both `python -m avro` and just `avro` without having an out-of-band `scripts/avro` directory.
- Just in case someone is still using scripts/avro, I've kept that around with a deprecation warning.
- I added type hints as best I can; however, the avro script has an `eval` call that we might want to get rid of, some day.

lang/py/avro/datafile.py
@@ -24,8 +24,9 @@ https://avro.apache.org/docs/current/spec.html#Object+Container+Files
 """
 import io
 import json
+import warnings
 from types import TracebackType
-from typing import BinaryIO, MutableMapping, Optional, Type, cast
+from typing import IO, AnyStr, BinaryIO, MutableMapping, Optional, Type, cast
 
 import avro.codecs
 import avro.errors
@@ -38,18 +39,21 @@ MAGIC = bytes(b"Obj" + bytearray([VERSION]))
 MAGIC_SIZE = len(MAGIC)
 SYNC_SIZE = 16
 SYNC_INTERVAL = 4000 * SYNC_SIZE  # TODO(hammer): make configurable
-META_SCHEMA: avro.schema.RecordSchema = avro.schema.parse(
-    json.dumps(
-        {
-            "type": "record",
-            "name": "org.apache.avro.file.Header",
-            "fields": [
-                {"name": "magic", "type": {"type": "fixed", "name": "magic", "size": MAGIC_SIZE}},
-                {"name": "meta", "type": {"type": "map", "values": "bytes"}},
-                {"name": "sync", "type": {"type": "fixed", "name": "sync", "size": SYNC_SIZE}},
-            ],
-        }
-    )
+META_SCHEMA = cast(
+    avro.schema.RecordSchema,
+    avro.schema.parse(
+        json.dumps(
+            {
+                "type": "record",
+                "name": "org.apache.avro.file.Header",
+                "fields": [
+                    {"name": "magic", "type": {"type": "fixed", "name": "magic", "size": MAGIC_SIZE}},
+                    {"name": "meta", "type": {"type": "map", "values": "bytes"}},
+                    {"name": "sync", "type": {"type": "fixed", "name": "sync", "size": SYNC_SIZE}},
+                ],
+            }
+        )
+    ),
 )
 
 NULL_CODEC = "null"
@@ -161,11 +165,14 @@ class DataFileWriter(_DataFileMetadata):
     sync_marker: bytes
 
     def __init__(
-        self, writer: BinaryIO, datum_writer: avro.io.DatumWriter, writers_schema: Optional[avro.schema.Schema] = None, codec: str = NULL_CODEC
+        self, writer: IO[AnyStr], datum_writer: avro.io.DatumWriter, writers_schema: Optional[avro.schema.Schema] = None, codec: str = NULL_CODEC
     ) -> None:
         """If the schema is not present, presume we're appending."""
-        self._writer = writer
-        self._encoder = avro.io.BinaryEncoder(writer)
+        if hasattr(writer, "mode") and "b" not in writer.mode:
+            warnings.warn(avro.errors.AvroWarning(f"Writing binary data to a writer {writer!r} that's opened for text"))
+        bytes_writer = getattr(writer, "buffer", writer)
+        self._writer = bytes_writer
+        self._encoder = avro.io.BinaryEncoder(bytes_writer)
         self._datum_writer = datum_writer
         self._buffer_writer = io.BytesIO()
         self._buffer_encoder = avro.io.BinaryEncoder(self._buffer_writer)
@@ -307,9 +314,12 @@ class DataFileReader(_DataFileMetadata):
     # TODO(hammer): allow user to specify expected schema?
     # TODO(hammer): allow user to specify the encoder
 
-    def __init__(self, reader, datum_reader):
-        self._reader = reader
-        self._raw_decoder = avro.io.BinaryDecoder(reader)
+    def __init__(self, reader: IO[AnyStr], datum_reader: avro.io.DatumReader) -> None:
+        if "b" not in reader.mode:
+            warnings.warn(avro.errors.AvroWarning(f"Reader binary data from a reader {reader!r} that's opened for text"))
+        bytes_reader = getattr(reader, "buffer", reader)
+        self._reader = bytes_reader
+        self._raw_decoder = avro.io.BinaryDecoder(bytes_reader)
         self._datum_decoder = None  # Maybe reset at every block.
         self._datum_reader = datum_reader
 

lang/py/avro/__main__.py
@@ -1,0 +1,276 @@
+#!/usr/bin/env python3
+
+##
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Command line utility for reading and writing Avro files."""
+import argparse
+import csv
+import functools
+import itertools
+import json
+import sys
+import warnings
+from pathlib import Path
+from typing import (
+    IO,
+    Any,
+    AnyStr,
+    Callable,
+    Collection,
+    Dict,
+    Generator,
+    Iterable,
+    Mapping,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+    cast,
+)
+
+import avro
+import avro.datafile
+import avro.errors
+import avro.io
+import avro.schema
+
+
+def print_json(row: object) -> None:
+    print(json.dumps(row))
+
+
+def print_json_pretty(row: object) -> None:
+    """Pretty print JSON"""
+    # Need to work around https://bugs.python.org/issue16333
+    # where json.dumps leaves trailing spaces.
+    result = json.dumps(row, sort_keys=True, indent=4).replace(" \n", "\n")
+    print(result)
+
+
+_write_row = csv.writer(sys.stdout).writerow
+
+
+def print_csv(row: Mapping[str, object]) -> None:
+    # We sort the keys to the fields will be in the same place
+    # FIXME: Do we want to do it in schema order?
+    _write_row(row[key] for key in sorted(row))
+
+
+def select_printer(format_: str) -> Callable[[Mapping[str, object]], None]:
+    return {"json": print_json, "json-pretty": print_json_pretty, "csv": print_csv}[format_]
+
+
+def record_match(expr: Any, record: Any) -> Callable[[Mapping[str, object]], bool]:
+    warnings.warn(avro.errors.AvroWarning("There is no way to safely type check this."))
+    return cast(Callable[[Mapping[str, object]], bool], eval(expr, None, {"r": record}))
+
+
+def _passthrough_keys_filter(obj: Mapping[str, object]) -> Dict[str, object]:
+    return {**obj}
+
+
+def field_selector(fields: Collection[str]) -> Callable[[Mapping[str, object]], Dict[str, object]]:
+    if fields:
+
+        def keys_filter(obj: Mapping[str, object]) -> Dict[str, object]:
+            return {k: obj[k] for k in obj.keys() & set(fields)}
+
+        return keys_filter
+    return _passthrough_keys_filter
+
+
+def print_avro(
+    data_file_reader: avro.datafile.DataFileReader,
+    header: bool,
+    format_: str,
+    filter_: str,
+    skip: int = 0,
+    count: int = 0,
+    fields: Collection[str] = "",
+) -> None:
+    if header and format_ != "csv":
+        raise avro.errors.UsageError("--header applies only to CSV format")
+    predicate = functools.partial(record_match, filter_) if filter_ else None
+    avro_filtered = filter(predicate, data_file_reader)
+    avro_slice = itertools.islice(avro_filtered, skip, (skip + count) if count else None)
+    fs = field_selector(fields)
+    avro_fields = (fs(cast(Mapping[str, object], r)) for r in avro_slice)
+    avro_enum = enumerate(avro_fields)
+    printer = select_printer(format_)
+    for i, record in avro_enum:
+        if header and i == 0:
+            _write_row(sorted(record))
+        printer(record)
+
+
+def print_schema(data_file_reader: avro.datafile.DataFileReader) -> None:
+    print(json.dumps(json.loads(data_file_reader.schema), indent=4))
+
+
+def cat(
+    files: Sequence[IO[AnyStr]], print_schema_: bool, header: bool, format_: str, filter_: str, skip: int, count: int, fields: Collection[str]
+) -> int:
+    datum_reader = avro.io.DatumReader()
+    for file_ in files:
+        with avro.datafile.DataFileReader(file_, datum_reader) as data_file_reader:
+            if print_schema_:
+                print_schema(data_file_reader)
+                continue
+            print_avro(data_file_reader, header, format_, filter_, skip, count, fields)
+    return 0
+
+
+def iter_json(info: Iterable[AnyStr], _: Any) -> Generator[object, None, None]:
+    for i in info:
+        row = (i if isinstance(i, str) else cast(bytes, i).decode()).strip()
+        if row:
+            yield json.loads(row)
+
+
+def convert(value: str, field: avro.schema.Field) -> Union[int, float, str, bytes, bool, None]:
+    type_ = field.type.type
+    if type_ in ("int", "long"):
+        return int(value)
+    if type_ in ("float", "double"):
+        return float(value)
+    if type_ == "string":
+        return value
+    if type_ == "bytes":
+        return value.encode()
+    if type_ == "boolean":
+        return value.lower() in ("1", "t", "true")
+    if type_ == "null":
+        return None
+    if type_ == "union":
+        return convert_union(value, field)
+    raise avro.errors.UsageError("No valid conversion type")
+
+
+def convert_union(value: str, field: avro.schema.Field) -> Union[int, float, str, bytes, bool, None]:
+    for name in (s.name for s in field.type.schemas):
+        try:
+            return convert(value, name)
+        except ValueError:
+            continue
+    raise avro.errors.UsageError("Exhausted Union Schema without finding a match")
+
+
+def iter_csv(info: IO[AnyStr], schema: avro.schema.RecordSchema) -> Generator[Dict[str, object], None, None]:
+    header = [field.name for field in schema.fields]
+    for row in csv.reader(getattr(i, "decode", lambda: i)() for i in info):
+        values = [convert(v, f) for v, f in zip(row, schema.fields)]
+        yield dict(zip(header, values))
+
+
+def guess_input_type(io_: IO[AnyStr]) -> str:
+    ext = Path(io_.name).suffixes[0].lower()
+    extensions = {".json": "json", ".js": "json", ".csv": "csv"}
+    try:
+        return extensions[ext]
+    except KeyError as e:
+        raise avro.errors.UsageError("Can't guess input file type (not .json or .csv)") from e
+
+
+def write(schema: avro.schema.RecordSchema, output: IO[AnyStr], input_files: Sequence[IO[AnyStr]], input_type: Optional[str] = None) -> int:
+    input_type = input_type or guess_input_type(cast(IO[AnyStr], next(iter(input_files), sys.stdin)))
+    iter_records = {"json": iter_json, "csv": iter_csv}[input_type]
+    with avro.datafile.DataFileWriter(output, avro.io.DatumWriter(), schema) as writer:
+        for file_ in input_files:
+            for record in iter_records(file_, schema):
+                writer.append(record)
+    return 0
+
+
+def csv_arg(arg: Optional[str]) -> Optional[Tuple[str, ...]]:
+    return tuple(f.strip() for f in arg.split(",") if f.strip()) if (arg and arg.strip()) else None
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Display/write for Avro files")
+    parser.add_argument("--version", action="version", version=avro.__version__)
+    subparsers = parser.add_subparsers(required=True, dest="command") if sys.version_info >= (3, 7) else parser.add_subparsers(dest="command")
+    subparser_cat = subparsers.add_parser("cat")
+    subparser_cat.add_argument("--version", action="version", version=avro.__version__)
+    subparser_cat.add_argument(
+        "--count",
+        "-n",
+        default=None,
+        type=int,
+        help="number of records to print",
+    )
+    subparser_cat.add_argument(
+        "--skip",
+        "-s",
+        type=int,
+        default=0,
+        help="number of records to skip",
+    )
+    subparser_cat.add_argument(
+        "--format",
+        "-f",
+        default="json",
+        choices=("json", "csv", "json-pretty"),
+        help="record format",
+    )
+    subparser_cat.add_argument(
+        "--header",
+        "-H",
+        default=False,
+        action="store_true",
+        help="print CSV header",
+    )
+    subparser_cat.add_argument(
+        "--filter",
+        "-F",
+        default=None,
+        help="filter records (e.g. r['age']>1)",
+    )
+    subparser_cat.add_argument(
+        "--print-schema",
+        "-p",
+        action="store_true",
+        default=False,
+        help="print schema",
+    )
+    subparser_cat.add_argument("--fields", type=csv_arg, default=None, help="fields to show, comma separated (show all by default)")
+    subparser_cat.add_argument("files", nargs="*", type=argparse.FileType("rb"), help="Files to read", default=(sys.stdin,))
+    subparser_write = subparsers.add_parser("write")
+    subparser_write.add_argument("--version", action="version", version=avro.__version__)
+    subparser_write.add_argument("--schema", "-s", type=avro.schema.from_path, required=True, help="schema JSON file (required)")
+    subparser_write.add_argument(
+        "--input-type",
+        "-f",
+        choices=("json", "csv"),
+        default=None,
+        help="input file(s) type (json or csv)",
+    )
+    subparser_write.add_argument("--output", "-o", help="output file", type=argparse.FileType("wb"), default=sys.stdout)
+    subparser_write.add_argument("input_files", nargs="*", type=argparse.FileType("r"), help="Files to read", default=(sys.stdin,))
+    return parser.parse_args()
+
+
+def main() -> int:
+    args = _parse_args()
+    if args.command == "cat":
+        return cat(args.files, args.print_schema, args.header, args.format, args.filter, args.skip, args.count, args.fields)
+    if args.command == "write":
+        return write(args.schema, args.output, args.input_files, args.input_type)
+    raise avro.errors.UsageError(f"Unknown command - {args.command}")
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())

lang/py/avro/schema.py
@@ -44,10 +44,10 @@ import datetime
 import decimal
 import json
 import math
-import sys
 import uuid
 import warnings
-from typing import Mapping, MutableMapping, Optional, Sequence, cast
+from pathlib import Path
+from typing import Mapping, MutableMapping, Optional, Sequence, Union, cast
 
 import avro.constants
 import avro.errors
@@ -1189,27 +1189,22 @@ def make_avsc_object(json_data: object, names: Optional[avro.name.Names] = None,
     raise avro.errors.SchemaParseException(fail_msg)
 
 
-# TODO(hammer): make method for reading from a file?
-
-
-def parse(json_string, validate_enum_symbols=True):
+def parse(json_string: str, validate_enum_symbols: bool = True) -> Schema:
     """Constructs the Schema from the JSON text.
 
     @arg json_string: The json string of the schema to parse
     @arg validate_enum_symbols: If False, will allow enum symbols that are not valid Avro names.
     @return Schema
     """
-    # parse the JSON
     try:
         json_data = json.loads(json_string)
-    except Exception as e:
-        msg = f"Error parsing JSON: {json_string}, error = {e}"
-        new_exception = avro.errors.SchemaParseException(msg)
-        traceback = sys.exc_info()[2]
-        raise new_exception.with_traceback(traceback)
+    except json.decoder.JSONDecodeError as e:
+        raise avro.errors.SchemaParseException(f"Error parsing JSON: {json_string}, error = {e}") from e
+    return make_avsc_object(json_data, Names(), validate_enum_symbols)
 
-    # Initialize the names object
-    names = Names()
 
-    # construct the Avro Schema object
-    return make_avsc_object(json_data, names, validate_enum_symbols)
+def from_path(path: Union[Path, str], validate_enum_symbols: bool = True) -> Schema:
+    """
+    Constructs the Schema from a path to an avsc (json) file.
+    """
+    return parse(Path(path).read_text(), validate_enum_symbols)

lang/py/avro/test/gen_interop_data.py
@@ -18,15 +18,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
+import base64
+import io
+import json
 import os
-import sys
+from pathlib import Path
+from typing import IO, TextIO
 
 import avro.codecs
 import avro.datafile
 import avro.io
 import avro.schema
 
-NULL_CODEC = "null"
+NULL_CODEC = avro.datafile.NULL_CODEC
 CODECS_TO_VALIDATE = avro.codecs.KNOWN_CODECS.keys()
 
 DATUM = {
@@ -47,17 +52,49 @@ DATUM = {
 }
 
 
-def generate(schema_path, output_path):
-    with open(schema_path) as schema_file:
-        interop_schema = avro.schema.parse(schema_file.read())
-    for codec in CODECS_TO_VALIDATE:
-        filename = output_path
-        if codec != NULL_CODEC:
-            base, ext = os.path.splitext(output_path)
-            filename = base + "_" + codec + ext
-        with avro.datafile.DataFileWriter(open(filename, "wb"), avro.io.DatumWriter(), interop_schema, codec=codec) as dfw:
-            dfw.append(DATUM)
+def gen_data(codec: str, datum_writer: avro.io.DatumWriter, interop_schema: avro.schema.Schema) -> bytes:
+    with io.BytesIO() as file_, avro.datafile.DataFileWriter(file_, datum_writer, interop_schema, codec=codec) as dfw:
+        dfw.append(DATUM)
+        dfw.flush()
+        return file_.getvalue()
+
+
+def generate(schema_file: TextIO, output_path: IO) -> None:
+    interop_schema = avro.schema.parse(schema_file.read())
+    datum_writer = avro.io.DatumWriter()
+    output = ((codec, gen_data(codec, datum_writer, interop_schema)) for codec in CODECS_TO_VALIDATE)
+    if output_path.isatty():
+        json.dump({codec: base64.b64encode(data).decode() for codec, data in output}, output_path)
+        return
+    for codec, data in output:
+        if codec == NULL_CODEC:
+            output_path.write(data)
+            continue
+        base, ext = os.path.splitext(output_path.name)
+        Path(f"{base}_{codec}{ext}").write_bytes(data)
+
+
+def _parse_args() -> argparse.Namespace:
+    """Parse the command-line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("schema_path", type=argparse.FileType("r"))
+    parser.add_argument(
+        "output_path",
+        type=argparse.FileType("wb"),
+        help=(
+            "Write the different codec variants to these files. "
+            "Will append codec extensions to multiple files. "
+            "If '-', will output base64 encoded binary"
+        ),
+    )
+    return parser.parse_args()
+
+
+def main() -> int:
+    args = _parse_args()
+    generate(args.schema_path, args.output_path)
+    return 0
 
 
 if __name__ == "__main__":
-    generate(sys.argv[1], sys.argv[2])
+    raise SystemExit(main())

lang/py/avro/test/mock_tether_parent.py
@@ -17,6 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import http.server
 import sys
 from typing import Mapping
@@ -64,17 +65,21 @@ class MockParentHandler(http.server.BaseHTTPRequestHandler):
         resp_writer.write_framed_message(resp_body)
 
 
+def _parse_args() -> argparse.Namespace:
+    """Parse the command-line arguments"""
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(required=True, dest="command") if sys.version_info >= (3, 7) else parser.add_subparsers(dest="command")
+    subparser_start_server = subparsers.add_parser("start_server", help="Start the server")
+    subparser_start_server.add_argument("port", type=int)
+    return parser.parse_args()
+
+
 def main() -> None:
     global SERVER_ADDRESS
-
-    if len(sys.argv) != 3 or sys.argv[1].lower() != "start_server":
-        raise avro.errors.UsageError("Usage: mock_tether_parent start_server port")
-
-    try:
-        port = int(sys.argv[2])
-    except ValueError as e:
-        raise avro.errors.UsageError("Usage: mock_tether_parent start_server port") from e
-
+    args = _parse_args()
+    if args.command != "start_server":
+        raise NotImplementedError(f"{args.command} is not a known command")
+    port = args.port
     SERVER_ADDRESS = (SERVER_ADDRESS[0], port)
     print(f"mock_tether_parent: Launching Server on Port: {SERVER_ADDRESS[1]}")
 

lang/py/avro/test/sample_http_client.py
@@ -17,76 +17,65 @@
 #
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import argparse
+import json
 
-import sys
-
-import avro.errors
 import avro.ipc
 import avro.protocol
 
-MAIL_PROTOCOL_JSON = """\
-{"namespace": "example.proto",
- "protocol": "Mail",
-
- "types": [
-     {"name": "Message", "type": "record",
-      "fields": [
-          {"name": "to",   "type": "string"},
-          {"name": "from", "type": "string"},
-          {"name": "body", "type": "string"}
-      ]
-     }
- ],
-
- "messages": {
-     "send": {
-         "request": [{"name": "message", "type": "Message"}],
-         "response": "string"
-     },
-     "replay": {
-         "request": [],
-         "response": "string"
-     }
- }
-}
-"""
-MAIL_PROTOCOL = avro.protocol.parse(MAIL_PROTOCOL_JSON)
+MAIL_PROTOCOL = avro.protocol.parse(
+    json.dumps(
+        {
+            "namespace": "example.proto",
+            "protocol": "Mail",
+            "types": [
+                {
+                    "name": "Message",
+                    "type": "record",
+                    "fields": [{"name": "to", "type": "string"}, {"name": "from", "type": "string"}, {"name": "body", "type": "string"}],
+                }
+            ],
+            "messages": {
+                "send": {"request": [{"name": "message", "type": "Message"}], "response": "string"},
+                "replay": {"request": [], "response": "string"},
+            },
+        }
+    )
+)
 SERVER_HOST = "localhost"
 SERVER_PORT = 9090
 
 
-def make_requestor(server_host, server_port, protocol):
-    client = avro.ipc.HTTPTransceiver(SERVER_HOST, SERVER_PORT)
+def make_requestor(server_host: str, server_port: int, protocol: avro.protocol.Protocol) -> avro.ipc.Requestor:
+    client = avro.ipc.HTTPTransceiver(server_host, server_port)
     return avro.ipc.Requestor(protocol, client)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) not in [4, 5]:
-        raise avro.errors.UsageError("Usage: <to> <from> <body> [<count>]")
+def _parse_args() -> argparse.Namespace:
+    """Parse the command-line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("to", help="Who the message is to")
+    parser.add_argument("from_", help="Who the message is from")
+    parser.add_argument("body", help="The message body")
+    parser.add_argument("num_messages", type=int, default=1, help="The number of messages")
+    return parser.parse_args()
 
-    # client code - attach to the server and send a message
-    # fill in the Message record
-    message = dict()
-    message["to"] = sys.argv[1]
-    message["from"] = sys.argv[2]
-    message["body"] = sys.argv[3]
 
-    try:
-        num_messages = int(sys.argv[4])
-    except IndexError:
-        num_messages = 1
-
-    # build the parameters for the request
-    params = {}
-    params["message"] = message
-
+def main() -> int:
+    # client code - attach to the server and send a message fill in the Message record
+    args = _parse_args()
+    params = {"message": {"to": args.to, "from": args.from_, "body": args.body}}
     # send the requests and print the result
-    for msg_count in range(num_messages):
+    for msg_count in range(args.num_messages):
         requestor = make_requestor(SERVER_HOST, SERVER_PORT, MAIL_PROTOCOL)
         result = requestor.request("send", params)
-        print("Result: " + result)
-
+        print(f"Result: {result}")
     # try out a replay message
     requestor = make_requestor(SERVER_HOST, SERVER_PORT, MAIL_PROTOCOL)
     result = requestor.request("replay", dict())
-    print("Replay Result: " + result)
+    print(f"Replay Result: {result}")
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())

lang/py/avro/test/test_bench.py
@@ -27,7 +27,7 @@ import timeit
 import unittest
 import unittest.mock
 from pathlib import Path
-from typing import List, Mapping, Sequence
+from typing import Mapping, Sequence, cast
 
 import avro.datafile
 import avro.io
@@ -35,18 +35,21 @@ import avro.schema
 from avro.utils import randbytes
 
 TYPES = ("A", "CNAME")
-SCHEMA: avro.schema.RecordSchema = avro.schema.parse(
-    json.dumps(
-        {
-            "type": "record",
-            "name": "Query",
-            "fields": [
-                {"name": "query", "type": "string"},
-                {"name": "response", "type": "string"},
-                {"name": "type", "type": "string", "default": "A"},
-            ],
-        }
-    )
+SCHEMA = cast(
+    avro.schema.RecordSchema,
+    avro.schema.parse(
+        json.dumps(
+            {
+                "type": "record",
+                "name": "Query",
+                "fields": [
+                    {"name": "query", "type": "string"},
+                    {"name": "response", "type": "string"},
+                    {"name": "type", "type": "string", "default": "A"},
+                ],
+            }
+        )
+    ),
 )
 READER = avro.io.DatumReader(SCHEMA)
 WRITER = avro.io.DatumWriter(SCHEMA)

lang/py/avro/test/test_compatibility.py
@@ -403,7 +403,7 @@ RECORD1_WITH_ENUM_AB = parse(
         {
             "type": SchemaType.RECORD,
             "name": "Record1",
-            "fields": [{"name": "field1", "type": dict(ENUM1_AB_SCHEMA.to_json())}],
+            "fields": [{"name": "field1", "type": ENUM1_AB_SCHEMA.to_json()}],
         }
     )
 )
@@ -412,7 +412,7 @@ RECORD1_WITH_ENUM_ABC = parse(
         {
             "type": SchemaType.RECORD,
             "name": "Record1",
-            "fields": [{"name": "field1", "type": dict(ENUM1_ABC_SCHEMA.to_json())}],
+            "fields": [{"name": "field1", "type": ENUM1_ABC_SCHEMA.to_json()}],
         }
     )
 )

commit 9da86d46413719fcb6122806aa03e8c4c23cd6b3

Allow to use io.BytesIO with DataFileReader
Fixes: e8c61ba4 ("AVRO-3162: Use Argparse to Manage Arguments (#1270)")

lang/py/avro/datafile.py
@@ -315,7 +315,7 @@ class DataFileReader(_DataFileMetadata):
     # TODO(hammer): allow user to specify the encoder
 
     def __init__(self, reader: IO[AnyStr], datum_reader: avro.io.DatumReader) -> None:
-        if "b" not in reader.mode:
+        if hasattr(reader, "mode") and "b" not in reader.mode:
             warnings.warn(avro.errors.AvroWarning(f"Reader binary data from a reader {reader!r} that's opened for text"))
         bytes_reader = getattr(reader, "buffer", reader)
         self._reader = bytes_reader